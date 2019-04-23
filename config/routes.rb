c1ad52e74654eb5cd4d4a20dd6f089c15a840b4a
EC2 Default User <ec2-user@ip-172-31-26-231.ap-southeast-1.compute.internal>
2019-04-23 14:51:10 +0000

    Finish ch07

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,5 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup', to: 'users#new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #root 'application#hello'
-  
+  resources :users
 end
