5ba2863faea1b957ad641c291fba635d03831e8b
EC2 Default User <ec2-user@ip-172-31-26-231.ap-southeast-1.compute.internal>
2019-04-26 19:53:08 +0000

    Finish ch08

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  include SessionsHelper
+  
+  def new
+    
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email])
+    # => User object or false
+    if user && user.authenticate(params[:session][:password])
+      # Success
+      log_in user
+      redirect_to user
+    else
+      # Failure
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save # => Validation
       #Sucess
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
       # GET "/users/#{@user.id}" => show
